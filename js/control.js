4ff0d690f50b76d0c8354d9d7779952b817feaed
JTulio26 <144379528+JTulio26@users.noreply.github.com>
2026-06-01 12:55:57 -0600

    Update control.js

--- js/control.js
+++ js/control.js
@@ -1,49 +1,30 @@
-/* ===== MAPEOS ===== */
 const movimientosTexto = {
-  1: "Adelante",
-  2: "Atrás",
-  3: "Detener",
-  4: "Vuelta adelante derecha",
-  5: "Vuelta adelante izquierda",
-  6: "Vuelta atrás derecha",
-  7: "Vuelta atrás izquierda",
-  8: "Giro 90° derecha",
-  9: "Giro 90° izquierda",
-  10: "Giro 360° derecha",
-  11: "Giro 360° izquierda"
+  1: "Adelante", 2: "Atrás", 3: "Detener",
+  4: "Vuelta adelante derecha", 5: "Vuelta adelante izquierda",
+  6: "Vuelta atrás derecha", 7: "Vuelta atrás izquierda",
+  8: "Giro 90° derecha", 9: "Giro 90° izquierda",
+  10: "Giro 360° derecha", 11: "Giro 360° izquierda"
 };
 
-/* ===== ENVIAR MOVIMIENTO ===== */
 function enviarMovimiento(id) {
-  fetch("http://18.204.20.70:5000/api/movimientos/", {
+  fetch("http://44.217.103.65/api/movimientos/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ id_dispositivo: 1, id_movimiento: id, estatus: "Ejecutado" })
-  })
-  .then(r => r.json())
-  .then(data => console.log("Movimiento enviado:", data))
-  .catch(err => console.error("Error enviando movimiento:", err));
+  });
 }
 
-/* ===== DETENER MOVIMIENTO ===== */
-function detenerMovimiento() {
-  enviarMovimiento(3);
-}
+function detenerMovimiento() { enviarMovimiento(3); }
 
-/* ===== ENVIAR VELOCIDAD ===== */
 function enviarVelocidad(idVel) {
-  fetch("http://18.204.20.70:5000/api/velocidad/actual", {
+  fetch("http://44.217.103.65/api/velocidad/actual", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ id_velocidad: idVel })
-  })
-  .then(r => r.json())
-  .then(data => console.log("Velocidad enviada:", data))
-  .catch(err => console.error("Error enviando velocidad:", err));
+  });
 }
 
-/* ========= WEBSOCKET ========= */
-const ws = new WebSocket("ws://18.204.20.70:5500");
+const ws = new WebSocket("ws://44.217.103.65:443");
 
 ws.onmessage = (msg) => {
   let data;
@@ -51,7 +32,7 @@ ws.onmessage = (msg) => {
 
   if (data.event === "nuevo_movimiento") {
     document.getElementById("monitorMovimiento").innerHTML = `
-      <div><span class="tag">Movimiento:</span> ${movimientosTexto[data.id_movimiento] || "Desconocido"}</div>
+      <div><span class="tag">Movimiento:</span> ${movimientosTexto[data.id_movimiento]}</div>
       <div><span class="tag">Fecha:</span> ${data.fecha_evento}</div>
     `;
   }
